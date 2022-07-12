a2102514c3ff2b6c7a1bc3c0afacbf57e3df5bb0
stellakeany <71975148+stellakeany@users.noreply.github.com>
2022-07-12 15:20:26 +0100

    Skill bar animation
    Fixed animation on progress bars in Skills tab so that the bars are animated to fill up every time the Skills tab is opened

--- 2.0/script.js
+++ 2.0/script.js
@@ -17,9 +17,70 @@ function openPage(pageName, elmnt) {
 
   // Add the specific color to the button used to open the tab content
   elmnt.style.backgroundColor = '#F3F4CD';
+
+  if (pageName === "Skills") {
+    setTimeout(animateProgress, 100);
+  } else{
+    resetProgress();
+  }
 }
 
 function loadDefault(){
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
 }
+
+function animateProgress(){
+  const bars = document.getElementsByClassName("inner-progress-bar");
+  var x;
+  
+  for (x = 0; x < bars.length; x++) {
+    var percentage = bars[x].id;
+    bars[x].style.width = percentage;
+  }
+
+}
+
+function resetProgress(){
+  const bars = document.getElementsByClassName("inner-progress-bar");
+  var x;
+  
+  for (x = 0; x < bars.length; x++) {
+    bars[x].style.width = '10%';
+  }
+}
+
+/*
+function typeWriter(tabID) {
+  var speed = 1;
+  var typeText;
+
+  switch(tabID){
+    case "Home":
+      typeText = "Hello World..."
+      break;
+    case "Projects":
+      typeText = "Projects"
+      break;
+    case "Education":
+      typeText = "Education"
+      break;
+    case "Skills":
+      typeText = "Skills"
+      break;
+  }
+
+  var tab = document.getElementById(tabID);
+  var title = tab.getElementsByTagName("h2");
+
+  type(0, typeText, title[0], speed);
+}
+
+function type(counter, text, element, speed){
+
+  if(counter < text.length){
+    element.innerHTML += text.charAt(counter);
+    counter++;
+    setTimeout(type(counter, text, element, speed), speed);
+  }
+}*/
