f80600e127ab91d313dd48c24b7af7f9a93d4a8e
stellakeany <stella.keany@ucdconnect.ie>
2024-06-06 15:41:56 +0100

    add globe

--- portfolio/src/components/Window.tsx
+++ portfolio/src/components/Window.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import TabBar from './TabBar';
-import exitIcon from '../assets/exit.svg';
+import globeIcon from '../assets/globe.svg';
 
 const Window: React.FC = () => {
   const [activeTab, setActiveTab] = useState<number>(0);
@@ -10,7 +10,10 @@ const Window: React.FC = () => {
     <div className="relative max-w-4xl mx-auto mt-10 before:content-[''] before:absolute before:top-3 before:left-3 before:w-full before:h-full before:bg-blue-dark before:rounded-lg before:z-0">
       <div className="relative border-2 border-black rounded-lg bg-white z-10">
         <div className="flex items-center justify-between bg-gray-light px-2 pt-2 rounded-t-lg border-b-2 border-black">
-          <TabBar tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
+          <div className="flex space-x-2">
+            <img src={globeIcon} alt="Globe" className="w-8 h-8 mb-2" />
+            <TabBar tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
+          </div>
           <div className="flex space-x-2">
             <button className="w-8 h-8 mb-2 border-2 border-black bg-red rounded-lg text-white flex items-center justify-center hover:bg-red-light">
               X
